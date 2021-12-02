bfc2d0a929f945febcf0f4081825e89ef41f430e
jngo2 <james.ngo@toronto.ca>
2021-12-01 22:43:24 -0500

    Refactor test

--- test.js
+++ test.js
@@ -2,28 +2,26 @@ const httpsRequest = require('./index.js');
 
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 
-Promise.resolve().then(() => {
+httpsRequest({
+	headers: { 'Accept': 'application/json' },
+	host: 'httpbin.org',
+	method: 'GET',
+	path: '/get'
+}).then(({ response: { statusCode }, data }) => {
 	console.log('\u001b[36mGET\u001b[0m');
+	console.log(statusCode, JSON.parse(data));
+}, (error) => {
+	console.error(error);
+});
 
-	return httpsRequest({
-		headers: { 'Accept': 'application/json' },
-		host: 'httpbin.org',
-		method: 'GET',
-		path: '/get'
-	}).then(({ response: { statusCode }, data }) => {
-		console.log(statusCode, JSON.parse(data));
-	});
-}).then(() => {
+httpsRequest({
+	headers: { 'Accept': 'application/json' },
+	host: 'httpbin.org',
+	method: 'POST',
+	path: '/post'
+}, { test: 'test' }).then(({ response: { statusCode }, data }) => {
 	console.log('\u001b[36mPOST\u001b[0m');
-
-	return httpsRequest({
-		headers: { 'Accept': 'application/json' },
-		host: 'httpbin.org',
-		method: 'POST',
-		path: '/post'
-	}, { test: 'test' }).then(({ response: { statusCode }, data }) => {
-		console.log(statusCode, JSON.parse(data));
-	});
-}).catch((error) => {
+	console.log(statusCode, JSON.parse(data));
+}, (error) => {
 	console.error(error);
 });
